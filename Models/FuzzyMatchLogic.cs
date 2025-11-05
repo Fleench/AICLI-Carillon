c5b0a811a39f759d3066585995da3a8784e8685d
Flench04 <47568518+Fleench@users.noreply.github.com>
2025-11-04 22:37:41 -0700

    Create FuzzyMatchLogic.cs

---
+++ Models/FuzzyMatchLogic.cs
@@ -0,0 +1,15 @@
+/* File: FuzzyMatchLogic.Cs
+ * Author: Glenn Sutherland
+ * Description: Finds songs that may be the same song and assigns them
+ *              the same internal songID. If the system is unsure then
+ *              it saves it and allows the user to decide for themselves.
+ */
+
+namespace Spotify_Playlist_Manager.Models
+{
+    public static class FuzzyMatchLogic
+    {
+        
+    }
+}
+
